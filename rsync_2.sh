17bab2276e3ab51ee49f92c0fb954513f441c2ea
Victor Solaris <victorsolaris095@gmail.com>
2020-11-15 13:36:41 -0800

    rsync delete flag

---
+++ rsync_2.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# [1] Synchronize the contents of destination directory with the source directory.
+# [2] Delete the contents of destination directory if it's not in the source. 
+
+# Options 
+# [ -r ]: recursive
+# [ -v ]: verbose
+# [ -n ]: dryrun
+
+# Summary
+# - dry run
+# - actual
+
+# dry run
+#rsync -rnv --delete /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
+
+# actual
+rsync -r --delete /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
