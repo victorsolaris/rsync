2f6dbb4a94a6495e59e51b29d85f57a55adb70d3
Victor Solaris <victorsolaris095@gmail.com>
2020-11-15 13:47:18 -0800

    minor edits

--- rsync_1.sh
+++ rsync_1.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
-# Synchronize the contents of destination directory with the
-# source directory
+# [1] Synchronize the contents of destination directory with the source directory
+# [2] Recursive
 
 # Options 
 # [ -r ]: recursive
 
+# Summary
+# - dry run
+# - actual
+
+# dry run
+rsync -rnv /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
+
+# actual
 rsync -r /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
+
