eb50ccf2e7b74b06b960e8f72cb037c8a010a690
Victor Solaris <victorsolaris095@gmail.com>
2020-11-15 14:28:14 -0800

    backup directory, date + time stamp, keep old logs

--- rsync_4.sh
+++ rsync_4.sh
@@ -15,6 +15,9 @@
 # - dry run
 # - create a backup file
 # - create a backup directory
+# - create a backup directory, use a file suffix
+# - create a backup directory, new directory
+# - create a backup direcotyr, don't delete old backups
 
 # dry run
 # rsync -abnv /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
@@ -28,12 +31,25 @@
 # create a backup directory in the the destination directory
 # rsync -ab --backup-dir=backup /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
 
-# create a backup directory
+# create a backup directory, use file suffix
 # create a backup directory in the the destination directory
 # use a ~ at the end of the file name to signifiy it is a backup
 # rsync -ab --backup-dir=backup --suffix=~ /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
 
+# create a backup directory, new directory
+# create a backup directory in the the destination directory
+# new directory each time, don't remove old one
+# backup directory uses date and time stamp
+# use a ~ at the end of the file name to signifiy it is a backup
+# rsync -ab --backup-dir="backup_$(date +%F_%T)" --suffix=~ /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
 
+
+# create a backup directory, don't delete old backups
+# create a backup directory in the the destination directory
+# new directory each time, don't remove old one
+# backup directory uses date and time stamp
+# use a ~ at the end of the file name to signifiy it is a backup
+# rsync -ab --backup-dir="backup_$(date +%F_%T)" --suffix=~ --delete --exclude="backup*" /home/user01/Documents/rsync/source-dir/ /home/user01/Documents/rsync/destination-dir/
 
 
 
