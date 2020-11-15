d9fe3f700c8a90ca6235f3f7002bec18b1c709a6
Victor Solaris <victorsolaris095@gmail.com>
2020-11-15 14:46:22 -0800

    compression

--- rsync_5.sh
+++ rsync_5.sh
@@ -13,16 +13,23 @@
 # [ -a ]: archive + recursive
 # [ -v ]: verbose
 # [ -n ]: dryrun
+# [ -z ]: compression
 
 # Summary
 # - dry run
 # - remote source
+# - remote source, compression
 
 # dry run
 # rsync -anv user01@192.168.10.2:/share/source-client2 /home/user01/Documents/rsync/destination-dir
 
 # remote source
-rsync -a --delete user01@192.168.10.2:/share/source-client2/ /home/user01/Documents/rsync/destination-dir
+# rsync -a --delete user01@192.168.10.2:/share/source-client2/ /home/user01/Documents/rsync/destination-dir
+
+# remote source
+# compression
+rsync -az --delete user01@192.168.10.2:/share/source-client2/ /home/user01/Documents/rsync/destination-dir
+
 
 
 
